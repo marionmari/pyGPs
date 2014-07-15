385e6985d2f822ca151d416e6c614a272665b0d0
Shan Huang <schan.huang@gmail.com>
2014-07-15 15:05:58 +0200

    new pyGPs version in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyGPs',
-    version='1.1',
+    version='1.2',
     author=['Marion Neumann',  'Shan Huang','Daniel Marthaler', 'Kristian Kersting'],
     author_email=['marion.neumann@uni-bonn.de.com', 'shan.huang@iais.fraunhofer.de', 'marthaler@ge.com', 'kristian.kersting@cs.tu-dortmund.de'],
     packages=find_packages(),
