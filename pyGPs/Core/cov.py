added unit tests for SM and Gabor covariance. test_model has corrected demo folder structure. test_inf and test_lik changed proceed to evaluate

pyGPs/Core/cov.py
@@ -441,9 +441,12 @@ class SM(Kernel):
     :param log_v: spectral variances.
     '''
 
-    def __init__(self, Q=0, hyps=[]):
-        self.hyp = hyps
-        self.para = [Q]
+    def __init__(self, Q=0, hyps=[], D=None):
+        if D:
+            self.hyp = np.random.random(Q*(1+2*D))
+        else:
+	        self.hyp = hyps
+    	self.para = [Q]
 
     def getCovMatrix(self,x=None,z=None,mode=None):
         Q = self.para[0]
@@ -1016,11 +1019,8 @@ class Matern(Kernel):
             return t
         elif d == 5:
             return t*(1+t)/3.
-<<<<<<< HEAD
-=======
         elif d == 7:
             return 1./(2.*t)*(1 + t/5.*(3. + 0.75*t + (t*t)/12.) )
->>>>>>> e4d4ba68b7a40c08350137c07ccc98d6f2d0282a
         else:
             raise Exception("Wrong value for d in Matern")
 

pyGPs/Testing/unit_test_lik.py
@@ -20,7 +20,7 @@ class LikTests(unittest.TestCase):
     def setUp(self):
         # random data for testing
         self.y = np.random.normal(loc=0.0, scale=1.0, size=(1,1))    # tiled target
-        self.mu = np.random.normal(loc=0.0, scale=1.0, size=(1,1))   # tiled conditional mean 
+        self.mu = np.random.normal(loc=0.0, scale=1.0, size=(1,1))   # tiled conditional mean
         self.s2 = np.random.normal(loc=0.0, scale=1.0, size=(1,1))   # tiled conditional variance
 
 
@@ -59,21 +59,21 @@ class LikTests(unittest.TestCase):
 
     def checkLikelihood(self, likelihood):
         print "predictive mode"
-        lp, ymu, ys2 = likelihood.proceed(y=self.y, mu=self.mu, s2=self.s2, nargout=3)
+        lp, ymu, ys2 = likelihood.evaluate(y=self.y, mu=self.mu, s2=self.s2, nargout=3)
         self.checkPrediction(lp, ymu, ys2)
 
         print "inference mode(Laplace inference)"
-        lp,dlp,d2lp,d3lp = likelihood.proceed(y=self.y, mu=self.mu, s2=self.s2, inffunc=pyGPs.inf.Laplace(), nargout=4)
+        lp,dlp,d2lp,d3lp = likelihood.evaluate(y=self.y, mu=self.mu, s2=self.s2, inffunc=pyGPs.inf.Laplace(), nargout=4)
         self.checkInferenceLaplace(lp,dlp,d2lp,d3lp)
         for der in xrange(len(likelihood.hyp)):
-            lp_dhyp,dlp_dhyp,d2lp_dhyp = likelihood.proceed(y=self.y, mu=self.mu, s2=self.s2, inffunc=pyGPs.inf.Laplace(), der=der, nargout=4)
+            lp_dhyp,dlp_dhyp,d2lp_dhyp = likelihood.evaluate(y=self.y, mu=self.mu, s2=self.s2, inffunc=pyGPs.inf.Laplace(), der=der, nargout=4)
             self.checkDerivativeLaplace(lp_dhyp,dlp_dhyp,d2lp_dhyp)
 
         print "inference mode(EP inference)"
-        lp,dlp,d2lp = likelihood.proceed(y=self.y, mu=self.mu, s2=self.s2, inffunc=pyGPs.inf.EP(), nargout=3)
+        lp,dlp,d2lp = likelihood.evaluate(y=self.y, mu=self.mu, s2=self.s2, inffunc=pyGPs.inf.EP(), nargout=3)
         self.checkInferenceEP(lp,dlp,d2lp)
         for der in xrange(len(likelihood.hyp)):
-            dlZhyp = likelihood.proceed(y=self.y, mu=self.mu, s2=self.s2, inffunc=pyGPs.inf.EP(), der=der, nargout=3)
+            dlZhyp = likelihood.evaluate(y=self.y, mu=self.mu, s2=self.s2, inffunc=pyGPs.inf.EP(), der=der, nargout=3)
             self.checkDerivativeEP(dlZhyp)
 
 